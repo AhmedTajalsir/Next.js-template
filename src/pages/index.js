e1947c28dddec871fe074acc8c0f928f2e80a191
AhmedTajalsir <ah.alkhier2@gmail.com>
2021-09-25 18:55:30 +0530

    init

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>App Name</title>
+      </Head>
+
+      {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
+      <center className="grid place-items-center mt-10">
+       
+        <h3 className="font-bold">
+          Tamplate using Next.js / Tailwind CSS / Redux / Firebase / NextAuth
+        </h3>
+       
+        
+
+        <p className="mt-24">Built with 💙 by Ahmed Tajalsir </p>
+      </center>
+      {/* ---- ---- */}
+    </div>
+  );
+}
